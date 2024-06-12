5746f322e7bc29cfa2ba66e7689ad50915ea26fd
Kaleb Berhane <kaleb.cpu@gmail.com>
2024-06-11 23:48:29 -0600

    navbar path updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./app.scss";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero"
 const App = () => {
