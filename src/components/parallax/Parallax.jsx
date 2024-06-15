055008ab588cde76248f946f76cf272ecbc7e7d9
jt-qwerty <janet.tsegazeab@gmail.com>
2024-06-15 03:32:28 -0600

    finished with background images for parallax effect

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./parallax.scss"
+import { motion } from 'framer-motion'
 
 const Parallax = () => {
     return (
         <div className="parallax" style={{background: "linear-gradient(180deg, #111132, #766e97"}}>
-           <h1> Welcome to My World! </h1>
+           <motion.h1> Welcome to My World! </motion.h1>
+
+           <motion.div className='mountains'> </motion.div>
+           <div className='planets'></div>
+           <div className='stars'></div>
+
 
 
         </div>
