483b1fc2bf3f9f4c4a91f583afb94ecc52171664
jt-qwerty <janet.tsegazeab@gmail.com>
2024-06-16 01:30:34 -0600

    finished parallax section

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import "./parallax.scss"
-import { motion } from 'framer-motion'
+import React, { useRef } from 'react';
+import "./parallax.scss";
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Parallax = () => {
+    const ref = useRef();
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end start"]
+    });
+
+    const yText = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+    const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "150%"]);
+
     return (
-        <div className="parallax" style={{background: "linear-gradient(180deg, #111132, #766e97"}}>
-           <motion.h1> Welcome to My World! </motion.h1>
-
-           <motion.div className='mountains'> </motion.div>
-           <div className='planets'></div>
-           <div className='stars'></div>
-
-
-
+        <div className="parallax" ref={ref} style={{ background: "linear-gradient(180deg, #111132, #766e97)" }}>
+            <motion.h1 style={{ y: yText }}> Welcome to My World! </motion.h1>
+            <motion.div className='mountains'></motion.div>
+            <motion.div className='planets' style={{ y: yBg}}></motion.div>
+            <motion.div className='stars'   style={{ x: yBg}}></motion.div>
         </div>
-    )
+    );
 }
 
-export default Parallax; 
+export default Parallax;
